4258179a7bd383cbe146e0b42994b9f4053fc729
Isak-Evaldsson <isak.evaldsson@outlook.com>
2021-07-31 16:52:49 +0200

    Improved config installation process

--- .dotfiles-scripts/install_dotfiles.sh
+++ .dotfiles-scripts/install_dotfiles.sh
@@ -1,9 +1,18 @@
+#
+# Install script when managing your dotfiles using the git bare method
+# modified version of a script by Nicola Paolucci (see article at https://www.atlassian.com/git/tutorials/dotfiles)
+#
+
+# Defining some variables
+bare_repo_dir=.dotfiles
+backup_dir=.config-backup
+
 # Cloning repo
-git clone --bare https://github.com/Isak-Evaldsson/dotfiles $HOME/.dotfiles
+git clone --bare https://github.com/Isak-Evaldsson/dotfiles $HOME/$bare_repo_dir
 
 # Temporary alias 
 function config {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$HOME/$bare_repo_dir/ --work-tree=$HOME $@
 }
 
 # Tries to checkout, if unsuccesfull backup older configs
@@ -12,8 +21,8 @@ if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    mkdir -p .config-backup
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    mkdir -p $backup_dir
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $backup_dir/{}
 fi;
 
 # Force backed-up old config files to be overwritten
--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 A backup of my dotfiles using the bare repo method (https://www.atlassian.com/git/tutorials/dotfiles)
 
 # Install dotfiles to a computer:
-Simply run:
-``` curl -s https://raw.githubusercontent.com/Isak-Evaldsson/dotfiles/master/.dotfiles-scripts/install_dotfiles.sh | /bin/bash```
+Is easiest done by runing the install script in .dotfiles-scripts dir, run the command below to fetch and run the install script
+```
+curl -s https://raw.githubusercontent.com/Isak-Evaldsson/dotfiles/master/.dotfiles-scripts/install_dotfiles.sh | /bin/bash
+```
