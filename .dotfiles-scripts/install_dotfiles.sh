e5782e5e9549ce7b089e0510d36bca9cc531fe3d
Isak Evaldsson <43935906+Isak-Evaldsson@users.noreply.github.com>
2021-07-31 14:28:57 +0200

    Update install_dotfiles.sh

--- .dotfiles-scripts/install_dotfiles.sh
+++ .dotfiles-scripts/install_dotfiles.sh
@@ -1,14 +1,23 @@
+# Cloning repo
 git clone --bare https://github.com/Isak-Evaldsson/dotfiles $HOME/.cfg
+
+# Temporary alias 
 function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
+
+# Tries to checkout, if unsuccesfull backup older configs
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
+    mkdir -p .config-backup
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
+
+# Force backed-up old config files to be overwritten
 config checkout -f
+
+# Avoids nightmare when using config status
 config config status.showUntrackedFiles no
